78caf39d389fe5c680cf10ec924faeed0b5439c6
tjdwlsakfn <84087795+tjdwlsakfn@users.noreply.github.com>
2021-05-15 15:28:05 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import paper from '@material-ui/core/paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1000
+  }
+})
 
 const customers = [
   {
@@ -31,10 +49,22 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-   {
-  customers.map(c => {
+      <paper classeName={classes.table}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+   {customers.map(c => {
     return (
     <Customer
     key={c.key}
@@ -48,9 +78,11 @@ class App extends Component {
       );
   })
    }
-      </div>
+   </TableBody>
+    </Table>
+      </paper>
   ); 
  }
 }
 
-export default App;
+export default withStyles(styles)(App);
